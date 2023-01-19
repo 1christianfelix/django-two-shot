eeb3f5b881329bdea20d6992645639a7241fc6a3
Christian Felix <1christianfelix@gmail.com>
2023-01-19 14:04:00 -0800

    Feature 6 complete

--- expenses/urls.py
+++ expenses/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
+
+def homepage(request):
+    return redirect("home")
 
 urlpatterns = [
+    path('', homepage),
     path("receipts/", include('receipts.urls')),
     path("admin/", admin.site.urls),
 ]
